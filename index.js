c44459c1b84243e323e0a30221cbea284f4cc3b1
Mustafa Abdillahi <mustafa.abdi1@outlook.com>
2021-11-25 20:31:46 +0000

    Solution for calculateBill

---
+++ index.js
@@ -0,0 +1,12 @@
+const calculateBill = (price, vat, tip) => {
+    if (
+      typeof price !== "number" ||
+      typeof vat !== "number" ||
+      typeof tip !== "number"
+    )
+      return "error";
+  
+    return "£" + (price + (price * (vat / 100) + tip));
+  };
+  
+  module.exports = calculateBill;
